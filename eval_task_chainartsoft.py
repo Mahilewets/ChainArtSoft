5d15d546770726a80deba2bbf4dfd6a65c8865ee
Nikita Mogilevets <mahilewets@gmail.com>
2020-01-10 16:40:23 +0300

    Add files via upload

---
+++ eval_task_chainartsoft.py
@@ -0,0 +1,28 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('path')
+args = parser.parse_args()
+
+input_file = open(args.path, 'r')
+input_data = input_file.read()
+
+if len(input_data) == 0:
+    exit()
+
+msg = list()
+
+msg.append(input_data[0])
+
+for i in range(1, len(input_data)):
+    curr_ch = input_data[i]
+    if len(msg) == 0:
+        msg.append(curr_ch)
+    else:
+        prev_ch = msg[-1]
+        if curr_ch == prev_ch:
+            msg.pop()
+        else:
+            msg.append(curr_ch)
+
+print("".join(msg))
